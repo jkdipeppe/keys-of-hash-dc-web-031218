e601ff5b4c773abfbb709cde4d8b068195f2bbd0
github username <github email address>
2018-03-07 14:12:54 -0500

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,22 @@
+require 'pry'
+
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    array = []
+
+    i = 0
+
+    while i < arguments.length
+      each {|animal, location| #why just each? found the solution but dont quite get it
+        if location == arguments[i] #when im in pry it says animal and location undefined
+          array << animal
+        end
+      }
+      i = i + 1
+      # binding.pry
+    end
+
+    array
   end
 end
